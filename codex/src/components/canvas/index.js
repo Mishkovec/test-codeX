4afc33674ee7c723de9219bd39e9612c8a717962
Mishkovec <neilsa@mail.ru>
2019-12-03 16:39:39 +0300

    basic layout

--- codex/src/components/canvas/index.js
+++ codex/src/components/canvas/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from './canvas.module.css';
 
-export default class Canvas extends React.Component {
+
+let createField = () => {    
+    let children = [];
+        for (let i = 0; i < 1024; i++) {
+            children.push(React.createElement('div', null, null));
+        }
+    return children;
+}
+
+
+export default class Canvas extends React.Component {    
     render() {
         return (
-            <div className={styles.canvas_wrapper}>
-                <div className={styles.canvas_point}></div>
+            <div 
+                className={styles.canvas_wrapper}
+                onClick={(e)=>{e.target.style.background = 'black'}}
+            > 
+                {createField()}               
             </div>
         )
     }
